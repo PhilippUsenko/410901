0160d79bc787047d18dbc73e26bfd2e183edf11e
EgorMeshcheryakov1 <omoforiks@gmail.com>
2024-10-14 19:22:33 +0300

    Create laba4.2.cpp

---
+++ Мещеряков/laba4/laba4.2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int arr[5][5]{};
+	int max[5]{};
+
+	cout << "Vvedite elmenty massiva: " << endl;
+	for (int i=0;i<5;i++){
+		for (int j = 0; j < 5; j++) {
+			cout << "[" << i + 1 << "]";
+			cout << "[" << j + 1 << "]";
+			cin >> arr[i][j];
+		}
+	}
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (arr[j][i] > max[i]) {
+				max[i] = arr[j][i];
+			}
+		}
+	}
+
+	int min = max[0];
+	for (int i = 0; i < 5; i++) {
+		if (max[i] < min) {
+			min = max[i];
+		}
+	}
+	cout << "Minimalny iz maksimalnych " << min;
+	return 0;
+}
