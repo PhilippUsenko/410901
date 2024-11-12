1d1e9fe109d00b4d433fbe3e447b72a1b6e93d58
Drivni <abakumov.gena@gmail.com>
2024-11-12 22:19:41 +0300

    Update main.cpp

--- Абакумов/lab2/main.cpp
+++ Абакумов/lab2/main.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
 
+int operations_with_numbers(int num) { //функция производит операцию над числом
+	if (num == 0) return 0;
+	else if (num < 0) return num*num;
+	else if (num > 0) return num*num*num;
+}
+
+int main() {
+	int number1, number2, number3;
+
+	cout << "Enter the first number: " << endl;
+	cin >> number1;
+
+	cout << "Enter the second number: " << endl;
+	cin >> number2;
+
+	cout << "Enter the third number: " << endl;
+	cin >> number3;
+
+
+	number1 = operations_with_numbers(number1);
+	number2 = operations_with_numbers(number2);
+	number3 = operations_with_numbers(number3);
+
+	
+	cout << "\n" << "The first number: " << number1 << endl;
+	cout << "The second number: " << number2 << endl;
+	cout << "The third number: " << number3 << endl;
+
+
+	system("pause>nul");
+	return 0;
+}
