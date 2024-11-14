16925e1ee3d43bc1dd368807d9d4363505537f4f
Eugene <ucta456@gmail.com>
2024-11-14 22:27:08 +0300

    Лобач, 2 и 3 лабы

---
+++ Лобач/Laba3/1/laba3-1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <cmath>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Введите целое n\n";
+
+    int n;
+
+    float x = 0;
+
+    while (!(std::cin >> n))
+    {
+        std::cout << "Некорректный ввод\n";
+        std::cin.clear();
+        std::cin.ignore(1000, '\n');
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        x += (pow(-1, i) * (i + 1)) / (i);
+    }
+
+    std::cout << "Для n = " << n << ", " << "x = " << x << std::endl;
+
+    return 0;
+}
