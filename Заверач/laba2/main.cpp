feeddc0ffef0ac6273825cab5cd53815deca3add
zaverachsonya <zaverachsonya18@gmail.com>
2024-12-09 23:06:56 +0300

    laba2

---
+++ Заверач/laba2/main.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+ setlocale(LC_ALL, "Russian"); 
+ float OX, OY; 
+  cout << "введите координату X "; 
+  cin >> OX; 
+  cout << "введите координату Y "; 
+  cin >> OY; 
+  if (OX > 0 && OY > 0) 
+   cout << "Первая четверть"; 
+  else  
+   if (OX < 0 && OY > 0) 
+    cout << "Вторая четверть"; 
+   else  
+    if (OX < 0 && OY < 0) 
+     cout << "Третья четверть"; 
+    else 
+     cout << "Четверть четверь"; 
+return 0; 
+} 
